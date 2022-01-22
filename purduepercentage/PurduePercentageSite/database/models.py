e5312b74ffb06b8606ce2e87f3a3ee61a60a30ce
Jackson <jrmoffet@gmail.com>
2022-01-22 13:09:41 -0500

    update database models, use more foreign keys

--- purduepercentage/PurduePercentageSite/database/models.py
+++ purduepercentage/PurduePercentageSite/database/models.py
@@ -1,9 +1,9 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
 from django.db.models.base import Model
-
+from django.db.models.deletion import CASCADE
 from django.db.models.enums import TextChoices
-from django.db.models.fields import BooleanField, IntegerField, FloatField
+from django.db.models.fields import BooleanField, FloatField, IntegerField
 from django.db.models.fields import CharField
 from django.db.models.fields.related import ForeignKey
 from django.utils.translation import gettext_lazy
@@ -28,28 +28,43 @@ class Lifestyle(TextChoices):
     MODERATE = 'MO', gettext_lazy('MODERATE')
     ACTIVE = 'AC', gettext_lazy('ACTIVE')
 
+class CourseListing(Model):
+    department = CharField(max_length=255)
+    course_number = CharField(max_length=255)
+    course_title = CharField(max_length=255)
+
+class Professor(Model):
+    department = CharField(max_length=255)
+    first = CharField(max_length=255)
+    last = CharField(max_length=255)
+
+class TeachingAssistant(Model):
+    department = CharField(max_length=255)
+    first = CharField(max_length=255)
+    last = CharField(max_length=255)
 
 class StudyMethod(Model):
     method = CharField(max_length=2, choices=StudyType.choices, default=StudyType.OTHER)
 
 class Exam(Model):
-    course = CharField(max_length=255, default=False)
+    course = ForeignKey(CourseListing, on_delete=CASCADE)
     exam = CharField(max_length=2, choices=ExamType.choices, default=ExamType.MIDTERM_1)
+    year = IntegerField(null=True)
     score = FloatField()
-    professor = CharField(max_length=255)
-    ta = CharField(max_length=255)
+    professor = ForeignKey(Professor, on_delete=CASCADE, null=True)
+    ta = ForeignKey(TeachingAssistant, on_delete=CASCADE, null=True)
     lec_attendance = BooleanField()
     rec_attendance = BooleanField()
-    study_method = ForeignKey(StudyMethod, on_delete=models.CASCADE)
+    study_method = ForeignKey(StudyMethod, on_delete=CASCADE, null=True)
     hrs_study = IntegerField()
-    major = CharField(max_length=255)
-    minor = CharField(max_length=255)
-    avg_sleep = IntegerField()
-    extracurricular_hours = IntegerField()
-    musician = BooleanField()
-    artist = BooleanField()
-    athlete = BooleanField()
-    lifestyle = CharField(max_length=2, choices=Lifestyle.choices, default=Lifestyle.MODERATE)
-    num_semesters = IntegerField()
+    major = CharField(max_length=255, null=True)
+    minor = CharField(max_length=255, null=True)
+    avg_sleep = IntegerField(null=True)
+    extracurricular_hours = IntegerField(null=True)
+    musician = BooleanField(null=True)
+    artist = BooleanField(null=True)
+    athlete = BooleanField(null=True)
+    lifestyle = CharField(max_length=2, choices=Lifestyle.choices, default=Lifestyle.MODERATE, null=True)
+    num_semesters = IntegerField(null=True)
     coop = BooleanField()
     retake = BooleanField()
