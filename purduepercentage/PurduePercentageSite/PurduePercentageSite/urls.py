a07a320085b0f2497e049c9e11b1e5e615faa125
samgoilo <slegoboy@gmail.com>
2022-01-22 12:45:40 -0500

    Added Sam's graphs and templates

--- purduepercentage/PurduePercentageSite/PurduePercentageSite/urls.py
+++ purduepercentage/PurduePercentageSite/PurduePercentageSite/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('pphomepage/', include('pphomepage.urls')),
     path('admin/', admin.site.urls),
+    path('graph/', include('graph.urls')),
 ]
